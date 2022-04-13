3f5a80aa2a69ae4b9e97b40ccd84a98cfdbfb98c
Miguel Cabral <44329689+Migueljfc@users.noreply.github.com>
2022-04-13 19:44:18 +0100

    App1 finished with a small error in the i2c

--- App1_I2C_PWM/main/i2c_simple_main.c
+++ App1_I2C_PWM/main/i2c_simple_main.c
@@ -5,6 +5,8 @@
 #include "esp_sleep.h"
 #include "driver/touch_pad.h"
 #include "esp_timer.h"
+#include "driver/adc.h"
+#include "driver/ledc.h"
 
 static const char *TAG = "TC74-I2C:";
 
@@ -21,12 +23,29 @@ static const char *TAG = "TC74-I2C:";
 
 #define SEC_MULTIPLIER 1000000l
 
+#define LEDC_GPIO 18     //LED GPIO pin
+static ledc_channel_config_t ledc_channel;  //LED control channel
+
 /**
  * @brief Initialize touchpad 
  */
 static void init_hw(void)
 {
-
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_8_BIT,
+        .freq_hz = 1000,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ledc_timer_config(&ledc_timer);
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.duty = 0;
+    ledc_channel.gpio_num = LEDC_GPIO;
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.hpoint = 0;
+    ledc_channel.timer_sel = LEDC_TIMER_0;
+    ledc_channel_config(&ledc_channel);
 }
 
 /**
@@ -73,7 +92,9 @@ void app_main(void)
         
         ESP_ERROR_CHECK(sensor_read(SENSOR_ADDR, data, 1));
         ESP_LOGI(TAG, "Val = %d", data[0]);    
-
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, data[0]);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+        vTaskDelay(500 / portTICK_RATE_MS);
     }
 
     ESP_ERROR_CHECK(i2c_driver_delete(I2C_MASTER_NUM));
