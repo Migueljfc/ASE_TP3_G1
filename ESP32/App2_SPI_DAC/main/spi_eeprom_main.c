6196e4eb95cbcb1ca9de2d5860ff4bcaa6d8e737
Hugo Moinheiro <hugo.moinheiro@ua.pt>
2022-04-18 16:13:26 +0100

    ex2

---
+++ ESP32/App2_SPI_DAC/main/spi_eeprom_main.c
@@ -0,0 +1,224 @@
+
+/* SPI Master Half Duplex EEPROM example.
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "spi_eeprom.h"
+
+#include <stddef.h>
+#include "esp_intr_alloc.h"
+#include "esp_attr.h"
+#include "driver/timer.h"
+
+/*
+ This code demonstrates how to use the SPI master half duplex mode to read/write a AT932C46D EEPROM (8-bit mode).
+*/
+
+#ifdef CONFIG_IDF_TARGET_ESP32
+#ifdef CONFIG_EXAMPLE_USE_SPI1_PINS
+#define EEPROM_HOST SPI1_HOST
+// Use default pins, same as the flash chip.
+#define PIN_NUM_MISO 7
+#define PIN_NUM_MOSI 8
+#define PIN_NUM_CLK 6
+#else
+#define EEPROM_HOST HSPI_HOST
+#define PIN_NUM_MISO 18
+#define PIN_NUM_MOSI 23
+#define PIN_NUM_CLK 19
+#endif
+
+#define PIN_NUM_CS 13
+#elif defined CONFIG_IDF_TARGET_ESP32S2
+#define EEPROM_HOST SPI2_HOST
+
+#define PIN_NUM_MISO 37
+#define PIN_NUM_MOSI 35
+#define PIN_NUM_CLK 36
+#define PIN_NUM_CS 34
+#elif defined CONFIG_IDF_TARGET_ESP32C3
+#define EEPROM_HOST SPI2_HOST
+
+#define PIN_NUM_MISO 2
+#define PIN_NUM_MOSI 7
+#define PIN_NUM_CLK 6
+#define PIN_NUM_CS 10
+
+#elif CONFIG_IDF_TARGET_ESP32S3
+#define EEPROM_HOST SPI2_HOST
+
+#define PIN_NUM_MISO 13
+#define PIN_NUM_MOSI 11
+#define PIN_NUM_CLK 12
+#define PIN_NUM_CS 10
+#endif
+
+#define Num_Samples  112
+#define MaxWaveTypes 3
+int i = 0;
+
+static const char TAG[] = "main";
+
+void setup() {
+  Serial.begin(115200);
+}
+
+static intr_handle_t s_timer_handle;
+
+static void timer_isr(void* arg) {
+    TIMERG0.int_clr_timers.t0 = 1;
+    TIMERG0.hw_timer[0].config.alarm_en = 1;
+
+    // your code, runs in the interrupt
+}
+
+void init_timer(int timer_period_us) {
+    timer_config_t config = {
+            .alarm_en = true,
+            .counter_en = false,
+            .intr_type = TIMER_INTR_LEVEL,
+            .counter_dir = TIMER_COUNT_UP,
+            .auto_reload = true,
+            .divider = 80   /* 1 us per tick */
+    };
+    
+    timer_init(TIMER_GROUP_0, TIMER_0, &config);
+    timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
+    timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, timer_period_us);
+    timer_enable_intr(TIMER_GROUP_0, TIMER_0);
+    timer_isr_register(TIMER_GROUP_0, TIMER_0, &timer_isr, NULL, 0, &s_timer_handle);
+
+    timer_start(TIMER_GROUP_0, TIMER_0);
+}
+
+void app_main(void) {
+    esp_err_t ret;
+#ifndef CONFIG_EXAMPLE_USE_SPI1_PINS
+    ESP_LOGI(TAG, "Initializing bus SPI%d...", EEPROM_HOST + 1);
+    spi_bus_config_t buscfg = {
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = PIN_NUM_MOSI,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 32,
+    };
+    // Initialize the SPI bus
+    ret = spi_bus_initialize(EEPROM_HOST, &buscfg, SPI_DMA_CH_AUTO);
+    ESP_ERROR_CHECK(ret);
+#else
+    ESP_LOGI(TAG, "Attach to main flash bus...");
+#endif
+
+    eeprom_config_t eeprom_config = {
+        .cs_io = PIN_NUM_CS,
+        .host = EEPROM_HOST,
+        .miso_io = PIN_NUM_MISO,
+    };
+#ifdef CONFIG_EXAMPLE_INTR_USED
+    eeprom_config.intr_used = true;
+    gpio_install_isr_service(0);
+#endif
+
+    eeprom_handle_t eeprom_handle;
+
+    ESP_LOGI(TAG, "Initializing device...");
+    ret = spi_eeprom_init(&eeprom_config, &eeprom_handle);
+    ESP_ERROR_CHECK(ret);
+
+    ret = spi_eeprom_write_enable(eeprom_handle);
+    ESP_ERROR_CHECK(ret);
+
+    // const char test_str[] = "Hello World!";
+    static byte WaveFormTable[MaxWaveTypes][Num_Samples] = {
+        // Sin wave
+        { 
+            0x80, 0x83, 0x87, 0x8A, 0x8E, 0x91, 0x95, 0x98, 0x9B, 0x9E, 0xA2, 0xA5, 0xA7, 0xAA, 0xAD, 0xAF,
+            0xB2, 0xB4, 0xB6, 0xB8, 0xB9, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF, 0xBF, 0xBF, 0xC0, 0xBF, 0xBF, 0xBF,
+            0xBE, 0xBD, 0xBC, 0xBB, 0xB9, 0xB8, 0xB6, 0xB4, 0xB2, 0xAF, 0xAD, 0xAA, 0xA7, 0xA5, 0xA2, 0x9E,
+            0x9B, 0x98, 0x95, 0x91, 0x8E, 0x8A, 0x87, 0x83, 0x80, 0x7C, 0x78, 0x75, 0x71, 0x6E, 0x6A, 0x67,
+            0x64, 0x61, 0x5D, 0x5A, 0x58, 0x55, 0x52, 0x50, 0x4D, 0x4B, 0x49, 0x47, 0x46, 0x44, 0x43, 0x42,
+            0x41, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x41, 0x42, 0x43, 0x44, 0x46, 0x47, 0x49, 0x4B,
+            0x4D, 0x50, 0x52, 0x55, 0x58, 0x5A, 0x5D, 0x61, 0x64, 0x67, 0x6A, 0x6E, 0x71, 0x75, 0x78, 0x7C
+        },
+        // Triangular wave
+        {
+            0x80, 0x84, 0x89, 0x8D, 0x92, 0x96, 0x9B, 0x9F, 0xA4, 0xA8, 0xAD, 0xB2, 0xB6, 0xBB, 0xBF, 0xC4,
+            0xC8, 0xCD, 0xD1, 0xD6, 0xDB, 0xDF, 0xE4, 0xE8, 0xED, 0xF1, 0xF6, 0xFA, 0xFF, 0xFA, 0xF6, 0xF1,
+            0xED, 0xE8, 0xE4, 0xDF, 0xDB, 0xD6, 0xD1, 0xCD, 0xC8, 0xC4, 0xBF, 0xBB, 0xB6, 0xB2, 0xAD, 0xA8,
+            0xA4, 0x9F, 0x9B, 0x96, 0x92, 0x8D, 0x89, 0x84, 0x7F, 0x7B, 0x76, 0x72, 0x6D, 0x69, 0x64, 0x60,
+            0x5B, 0x57, 0x52, 0x4D, 0x49, 0x44, 0x40, 0x3B, 0x37, 0x32, 0x2E, 0x29, 0x24, 0x20, 0x1B, 0x17,
+            0x12, 0x0E, 0x09, 0x05, 0x00, 0x05, 0x09, 0x0E, 0x12, 0x17, 0x1B, 0x20, 0x24, 0x29, 0x2E, 0x32,
+            0x37, 0x3B, 0x40, 0x44, 0x49, 0x4D, 0x52, 0x57, 0x5B, 0x60, 0x64, 0x69, 0x6D, 0x72, 0x76, 0x7B
+        },
+        // Square wave
+        {
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+            0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        }
+    };
+
+    // Writing
+    ESP_LOGI(TAG, "Write: ");
+    ESP_LOGI(TAG, str(WaveFormTable));
+    int count = 0;
+    for (int i = 0; i < MaxWaveTypes; i++) {
+        for (int j = 0; j < Num_Samples; j++) {
+            // No need for this EEPROM to erase before write.
+            ret = spi_eeprom_write(eeprom_handle, count, WaveFormTable[i][j]);
+            ESP_ERROR_CHECK(ret);
+            count++;
+        }
+    }
+
+    // Reading
+    static byte test_WaveFormTable[MaxWaveTypes][Num_Samples];
+    count = 0;
+    for (int i = 0; i < MaxWaveTypes; i++) {
+        for (int j = 0; j < Num_Samples; j++) {
+            ret = spi_eeprom_read(eeprom_handle, count, &test_WaveFormTables[i][j]);
+            ESP_ERROR_CHECK(ret);
+            count++;
+        }
+    }
+    ESP_LOGI(TAG, "Read: \n");
+    ESP_LOGI(TAG, str(test_WaveFormTables));
+
+    ESP_LOGI(TAG, "Writing and Reading fase over. Proceding to generate wave signals.");
+
+    int wave_type = 0;
+    while (1) {
+        init_timer(10000);
+        
+        dacWrite(25, WaveFormTable[wave_type][i]); 
+        i++;
+        if (i >= Num_Samples) i = 0; 
+
+        // TODO - when timer interrupts, change wave type
+        if () {
+            i = 0;
+            if (wave_type >= MaxWaveTypes) wave_type = 0;
+            else wave_type++;
+        }
+        // vTaskDelay(1);
+    }
+}
